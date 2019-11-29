a5853c99639e46f0c0d4d4a605e34a54abdd8c18
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2019-11-29 17:13:20 +0800

    Add files via upload
    对query.proto文件，使用protobuf 3.6.1，生成的C++生成类头文件query.pb.h和query.pb.cc以及相关的测试代码，在VS2017测试通过。

---
+++ C++/protobuf示例/main.cpp
@@ -0,0 +1,87 @@
+#include "config/query.pb.h"
+
+#include "../include/type.h"
+
+#include <string>
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+//typedef muduo::Query T;
+
+using namespace ::google::protobuf;
+
+typedef std::shared_ptr<google::protobuf::Message> MessagePtr;
+
+// createMessage(const std::string& typeName)返回的是动态创建的对象的指针，调用方有责任释放它，不然会出现内存泄漏
+google::protobuf::Message* createMessage(const std::string& typeName)
+{
+	google::protobuf::Message* message = NULL;
+	const google::protobuf::Descriptor* descriptor = 
+		google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(typeName);
+	if (descriptor)
+	{
+		const google::protobuf::Message* prototype =
+			google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+		if (prototype)
+		{
+			message = prototype->New();
+		}
+	}
+	return message;
+}
+
+
+void testQuery()
+{
+	muduo::Query query;
+	query.set_id(1);
+	query.set_questioner("Cao Chufeng");
+	query.set_question("Running?");
+
+	muduo::Answer answer;
+	answer.set_id(1);
+	answer.set_questioner("Cao Chufeng");
+	answer.set_answerer("blog.csdn.net/ccf19881030");
+	answer.add_solution("Jump!");
+	answer.add_solution("Win!");
+}
+
+int main(int argc, char* argv[])
+{
+	std::string type_name = muduo::Query::descriptor()->full_name();
+	std::cout << type_name << std::endl;
+
+	const ::google::protobuf::Descriptor* descriptor = ::google::protobuf::DescriptorPool::generated_pool()
+		->FindMessageTypeByName(type_name);
+	assert(descriptor == muduo::Query::descriptor());
+	cout << "FindMessageTypeByName() = " << descriptor << endl;
+	cout << "muduo::Query::descriptor() = " << muduo::Query::descriptor() << endl;
+	cout << endl;
+
+	const ::google::protobuf::Message* prototype = ::google::protobuf::MessageFactory::generated_factory()
+						->GetPrototype(descriptor);
+	assert(prototype == &muduo::Query::default_instance());
+	cout << "GetPrototype() = " << prototype << endl;
+	cout << "&muduo::Query::default_instance() = " << &muduo::Query::default_instance() << endl;
+	cout << endl;
+
+	muduo::Query* new_obj = dynamic_cast<muduo::Query*>(prototype->New());
+	assert(new_obj != NULL);
+	assert(new_obj != prototype);
+	assert(typeid(*new_obj) == typeid(muduo::Query::default_instance()));
+	cout << "prototype->New() = " << new_obj << endl;
+	cout << endl;
+	delete new_obj;
+
+	google::protobuf::Message* newQuery = createMessage("muduo.Query");
+	assert(newQuery != NULL);
+	assert(typeid(*newQuery) == typeid(muduo::Query::default_instance()));
+	cout << "createMessage(\"muduo.Query\") = " << newQuery << endl;
+
+	testQuery();
+
+	getchar();
+	
+	return 0;
+}
