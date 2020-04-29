a836c62a2deea10771047247f634882093c5dab7
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2020-04-29 14:46:33 +0800

    Create library.js

---
+++ JavaScript/JavaScript相关的库/library.js
@@ -0,0 +1,19 @@
+## Javascript相关的库
+### Javascript时间库
+* [Day.js](https://day.js.org/)
+Day.js 2KB immutable date library alternative to Moment.js with the same modern API https://day.js.org
+https://github.com/iamkun/dayjs/
+[在Nodejs中使用Day.js库](https://day.js.org/docs/zh-CN/installation/node-js)
+要在您的 Node.js 项目中使用 Day.js，只需使用 NPM 安装。
+```shell
+npm install dayjs --save
+```
+然后在项目代码中引入即可：
+```js
+var dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+```
+
+* [Moment.js-JavaScript 日期处理类库](http://momentjs.cn/)
+[Moment.js 中文文档](http://momentjs.cn/)
