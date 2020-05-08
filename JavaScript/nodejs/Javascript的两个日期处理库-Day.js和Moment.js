5805b32ac53aa9787b88ca19e0282fa22e631e2f
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2020-05-08 15:16:09 +0800

    Create Javascript的两个日期处理库-Day.js和Moment.js

---
+++ JavaScript/nodejs/Javascript的两个日期处理库-Day.js和Moment.js
@@ -0,0 +1,9 @@
+## 一、[Day.js 教程](https://github.com/apachecn/zetcode-zh/blob/master/docs/js/70.md)
+Day.js 教程展示了如何通过 Day.js 模块在 JavaScript 中使用日期和时间。
+原文：[http://zetcode.com/javascript/dayjs/](http://zetcode.com/javascript/dayjs/)
+Day.js 是用于解析，验证，操作和格式化日期的极简 JavaScript 日期库。 它是 Moment.js 的替代库，并且具有兼容的 API。 Moment.js 在 Moment.js 教程中进行了介绍。
+### [Day.js-中文文档](https://day.js.org/zh-CN/)
+## 二、[Moment.js 教程](https://github.com/ccf19881030/zetcode-zh/blob/master/docs/js/69.md)
+原文： [http://zetcode.com/javascript/momentjs/](http://zetcode.com/javascript/momentjs/)
+
+### [Moment.js-JavaScript 日期处理类库](http://momentjs.cn/)
